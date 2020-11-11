9c372c2a9984ef1714726810393b343b02e5b57d
damiel <daniel@users.noreply.github.com>
2020-11-11 16:58:26 +0800

    创建8001支付服务模块及启动类

---
+++ cloud-provider-payment8001/src/main/java/com/crudsavior/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.crudsavior.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * PaymentMain8001
+ *
+ * @author arctic
+ * @date 2020/11/11
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
